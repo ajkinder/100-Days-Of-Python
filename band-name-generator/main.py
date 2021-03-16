4f3c85cc9ca2aadc73eac0aa4467112cad8cf198
ajkinder <alexjohnkinder@gmail.com>
2021-03-15 17:55:00 -0700

    Second Commit. Tip Calculator and Band Name Generator completed. Day 1 and 2.

---
+++ band-name-generator/main.py
@@ -0,0 +1,10 @@
+# Band Name Generator
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("Welcome to the Band Name Generator.")
+    street = input("What's name of the city you grew up in?\n")
+    pet = input("What's your pet's name?\n")
+    print("Your band name could be " + street + " " + pet)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
