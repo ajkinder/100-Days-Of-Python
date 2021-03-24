f80f38ea7c908135393ee35c7a3b568e8079ef12
ajkinder <alexjohnkinder@gmail.com>
2021-03-24 15:23:09 -0700

    Casesar's Cipher encode and decode prodecures correctly encode and decode messages using Caeser's Cipher.

--- caesar-cipher/main.py
+++ caesar-cipher/main.py
@@ -2,9 +2,32 @@
 # Code and Decode messages using Caesar's Cipher
 # Written by: Alexander Kinder
 
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
 
 
+# MARK - Function declarations
+def encrypt(plain_message, shift_by):
+    encoded_message = ""
+    for letter in plain_message:
+        encoded_message += alphabet[alphabet.index(letter) + shift_by]
+    print(f"The encrypted text is {encoded_message}")
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+
+def decrypt(cypher_message, shift_by):
+    decoded_message = ""
+    for letter in cypher_message:
+        decoded_message += alphabet[alphabet.index(letter) - shift_by]
+    print(f"The decrpted text is {decoded_message}")
+
+
+if direction == "encode":
+    encrypt(plain_message=text, shift_by=shift)
+elif direction == "decode":
+    decrypt(cypher_message=text, shift_by=shift)
 
