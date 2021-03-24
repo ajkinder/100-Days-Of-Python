de74bd9e1e2748e243b70021b9ebc0ae96afd4d0
ajkinder <alexjohnkinder@gmail.com>
2021-03-24 15:52:17 -0700

    Caesar's Cipher: Added ASCII art. While loop to make encode and decode sequence infinite until user chooses to quit. Fixed bug that caused crash on large shift_by input sizes.

--- caesar-cipher/main.py
+++ caesar-cipher/main.py
@@ -2,14 +2,13 @@
 # Code and Decode messages using Caesar's Cipher
 # Written by: Alexander Kinder
 
+# IMPORTS
+import art
+
+# GLOBAL VARIABLES
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
             'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
 
 # MARK - Function declarations
 def caesar(input_text, shift_by, cipher_direction):
@@ -18,9 +17,27 @@ def caesar(input_text, shift_by, cipher_direction):
         shift_by *= -1
 
     for letter in input_text:
-        return_message += alphabet[alphabet.index(letter) + shift_by]
+        if letter in alphabet:
+            return_message += alphabet[alphabet.index(letter) + shift_by]
+        else:
+            return_message += letter
+
     print(f"The {cipher_direction}d text is {return_message}")
 
 
-# Call Caesar
-caesar(input_text=text, shift_by=shift, cipher_direction=direction)
+# BODY
+print(art.logo)
+
+is_running = True
+
+while is_running:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26  # adjustment for large shift inputs
+
+    caesar(input_text=text, shift_by=shift, cipher_direction=direction)
+
+    play_input = input("\nDo you want to encode or decode another message?\n").lower()
+    if play_input == "no":
+        is_running = False
