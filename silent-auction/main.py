01af38b40589567e9adda6e30ddacb2152cfbb69
ajkinder <alexjohnkinder@gmail.com>
2021-03-25 11:39:26 -0700

    Calculator: Added 6 operations to calculator. Added ASCII artwork. Added looping functionality to chain operations. Calculator application completed

--- silent-auction/main.py
+++ silent-auction/main.py
@@ -2,9 +2,36 @@
 # Turn based silent auction application that
 # allows you to have a silent auction with all of your friends.
 
+# IMPORTS
+import art
+import os
+clear = lambda: os.system('clear')
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print("Hello")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    for name in bidding_record:
+        bid_amount = bidding_record[name]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = name
+    print(f"The winner is {winner} with a final bit of ${highest_bid}.")
+
+
+# BODY
+print(art.logo)
+
+bids = {}
+is_bidding = True
+
+while is_bidding:
+    name = input("What is your name?: ")
+    bid = int(input("What is your bid?: $"))
+    bids[name] = bid
+    should_continue = input("Are there any more bidders? Type yes or no.").lower()
+    if should_continue == "no":
+        is_bidding = False
+    elif should_continue == "yes":
+        clear()
+
+find_highest_bidder(bids)
